f3ececfce3cc4126512316de9f05de8ef1e3abb4
Eduardo-201258 <201258@ids.upchiapas.edu.mx>
2023-07-02 18:31:09 -0600

    add docker-file

--- src/database/psql.ts
+++ src/database/psql.ts
@@ -1,10 +1,10 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  user: "postgres",
-  password: "eduardo",
-  host: "localhost",
-  database: "db_integrador",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_USER,
   port: 5432,
 });
 
